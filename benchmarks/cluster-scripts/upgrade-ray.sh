c1005ee93264d33f0e5d3c30470ed34dc103e428
Stephanie <swang@cs.berkeley.edu>
2018-04-20 20:21:41 +0000

    Cluster scripts

---
+++ benchmarks/cluster-scripts/upgrade-ray.sh
@@ -0,0 +1,21 @@
+# Make sure the SSH session has the correct version of Python on its path.
+# You will probably have to change the line below.
+export PATH=/home/ubuntu/anaconda3/bin/:$PATH
+# Do pushd/popd to make sure we end up in the same directory.
+pushd .
+# Upgrade Ray.
+cd ray
+git fetch origin
+git checkout xray-no-lineage-stash
+git reset --hard origin/xray-no-lineage-stash
+#git checkout fault-tolerance-tests
+#git checkout reconstruction-suppression-tests
+
+#rm -rf src/thirdparty/arrow
+#cd python
+#python setup.py develop
+
+cd python/ray/core
+make
+
+popd
