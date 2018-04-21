0f32afde82560f93772b6552b9faf2722bc4b56d
Stephanie <swang@cs.berkeley.edu>
2018-04-21 02:27:21 +0000

    debug

--- benchmarks/cluster-scripts/copy-ray-logs.sh
+++ benchmarks/cluster-scripts/copy-ray-logs.sh
@@ -4,4 +4,5 @@ rm -rf *-logs
 for worker in $(cat workers.txt); do
     mkdir $worker-logs
     scp -o StrictHostKeyChecking=no $worker:/tmp/raylogs/raylet_0* $worker-logs/
+    scp -o StrictHostKeyChecking=no $worker:/tmp/raylogs/plasma_store* $worker-logs/
 done
