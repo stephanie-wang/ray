730718b2e55c119da44f707d6723e4d6b5460a67
Stephanie <swang@cs.berkeley.edu>
2018-04-23 22:09:52 +0000

    Fix cluster scripts

--- benchmarks/cluster-scripts/run-trial.sh
+++ benchmarks/cluster-scripts/run-trial.sh
@@ -5,14 +5,14 @@ export PATH=/home/ubuntu/anaconda3/bin/:$PATH
 NUM_NODES=$1
 NUM_REDIS_SHARDS=$2
 GCS_DELAY_MS="${3:-"-1"}"
+HEAD=`head -n 1 workers.txt`
 
+./stop-ray.sh
 ./start-ray.sh $NUM_NODES $NUM_REDIS_SHARDS $GCS_DELAY_MS
 
 pushd .
 
-cd ~/ray/benchmarks
-python tree.py --num-recursions 12 --redis-address 172.30.0.164:6379 --use-raylet 2>&1 | tee "xray-$GCS_DELAY_MS-gcs-delay-$NUM_NODES-nodes-$NUM_REDIS_SHARDS-shards.out"
+cd ..
+python tree.py --num-recursions 12 --redis-address $HEAD:6379 --use-raylet 2>&1 | tee "xray-$GCS_DELAY_MS-gcs-delay-$NUM_NODES-nodes-$NUM_REDIS_SHARDS-shards.out"
 
 popd
-
-./stop-ray.sh
