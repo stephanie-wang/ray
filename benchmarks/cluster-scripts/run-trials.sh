ddc3b478260bd10d64f4884a31a736eadd4eb3ef
Stephanie <swang@cs.berkeley.edu>
2018-04-23 20:38:19 +0000

    Scripts to run trials

---
+++ benchmarks/cluster-scripts/run-trials.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUM_REDIS_SHARDS=$1
+
+for NUM_NODES in "$@";
+do
+    echo "Running lineage stash trials, with $NUM_NODES nodes"
+    ./run-trial.sh $NUM_NODES $NUM_REDIS_SHARDS
+done
+
+for NUM_NODES in "$@";
+do
+    echo "Running GCS-only trials, with $NUM_NODES nodes"
+    ./run-trial.sh $NUM_NODES $NUM_REDIS_SHARDS 0
+done
