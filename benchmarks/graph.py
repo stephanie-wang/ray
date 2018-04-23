0e79cfa236f81bada130033cf86c350734ee72a8
Stephanie <swang@cs.berkeley.edu>
2018-04-22 21:18:00 -0700

    Graph query script

--- benchmarks/graph.py
+++ benchmarks/graph.py
@@ -13,27 +13,31 @@ log.setLevel(logging.DEBUG)
 NUM_TRIALS = 30
 
 @ray.remote
-def ancestor(graph_id, node_index):
+def ancestor(graph_id, node_index, task_delay_ms):
     graph = ray.get(graph_id[0])
     node = graph[node_index]
+    time.sleep(float(task_delay_ms) / 1000)
+
     if node.parent is None:
         return [node.index]
 
-    ancestors = ray.get(ancestor.remote(graph_id, node.parent))
+    ancestors = ray.get(ancestor.remote(graph_id, node.parent, task_delay_ms))
     return [node.index] + ancestors
 
 @ray.remote
-def ancestors(graph_id, node_indices):
-    return [ancestor.remote(graph_id, node_index) for node_index in node_indices]
+def ancestors(graph_id, node_indices, task_delay_ms):
+    return [ancestor.remote(graph_id, node_index, task_delay_ms) for node_index in node_indices]
 
 @ray.remote
-def get_leaves(graph_id, node_index):
+def get_leaves(graph_id, node_index, task_delay_ms):
     graph = ray.get(graph_id[0])
     node = graph[node_index]
+    time.sleep(float(task_delay_ms) / 1000)
+
     if len(node.children) == 0:
         return [node.index]
 
-    children_leaves = ray.get([get_leaves.remote(graph_id, child) for child in node.children])
+    children_leaves = ray.get([get_leaves.remote(graph_id, child, task_delay_ms) for child in node.children])
     return [leaf for leaves in children_leaves for leaf in leaves]
 
 def make_tree(depth, degree):
@@ -62,6 +66,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--depth', type=int, required=True)
     parser.add_argument('--degree', type=int, required=True)
+    parser.add_argument('--task-delay-ms', type=int, default=1)
 
     args = parser.parse_args()
 
@@ -75,8 +80,8 @@ if __name__ == '__main__':
 
         start = time.time()
 
-        leaves = [get_leaves.remote([tree_id], 0)]
-        path_id_lists = ray.get([ancestors.remote([tree_id], leaf) for leaf in leaves])
+        leaves = [get_leaves.remote([tree_id], 0, args.task_delay_ms)]
+        path_id_lists = ray.get([ancestors.remote([tree_id], leaf, args.task_delay_ms) for leaf in leaves])
         path_ids = [path_id for path_id_list in path_id_lists for path_id in path_id_list]
         paths = ray.get(path_ids)
         log.info("Ray took %f seconds", time.time() - start)
