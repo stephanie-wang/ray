1d5b5acbf1eb1e93d90695c21fab991ffcfcd18b
Stephanie <swang@cs.berkeley.edu>
2018-04-26 00:49:36 +0000

    YSB, use mock event generator for now, batch events at project task before reduce step, oversubscribe CPUs

--- benchmarks/cluster-scripts/start-ray.sh
+++ benchmarks/cluster-scripts/start-ray.sh
@@ -12,9 +12,9 @@ HEAD=`head -n 1 workers.txt`
 
 rm /tmp/raylogs/*
 if [ -z "$GCS_DELAY_MS" ]; then
-    ray start --head --huge-pages --plasma-directory /mnt/hugepages --redis-port=6379 --resources='{"Node0": 1000}' --use-raylet --num-redis-shards $NUM_REDIS_SHARDS --use-task-shard
+    ray start --head --huge-pages --plasma-directory /mnt/hugepages --redis-port=6379 --resources='{"Node0": 1000}' --num-cpus 100 --use-raylet --num-redis-shards $NUM_REDIS_SHARDS --use-task-shard
 else
-    ray start --head --huge-pages --plasma-directory /mnt/hugepages --redis-port=6379 --resources='{"Node0": 1000}' --use-raylet --num-redis-shards $NUM_REDIS_SHARDS --use-task-shard --gcs-delay-ms $GCS_DELAY_MS
+    ray start --head --huge-pages --plasma-directory /mnt/hugepages --redis-port=6379 --resources='{"Node0": 1000}' --num-cpus 100 --use-raylet --num-redis-shards $NUM_REDIS_SHARDS --use-task-shard --gcs-delay-ms $GCS_DELAY_MS
 fi
 
 parallel-ssh -i -h <(tail -n +2 workers.txt) -x "-A -o StrictHostKeyChecking=no" -P "rm /tmp/raylogs/* || true"
