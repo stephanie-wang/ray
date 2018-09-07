940b7cce875af9d165ad59e478c3cd2562f916d1
Stephanie <swang@cs.berkeley.edu>
2018-09-07 01:06:15 +0000

    Use LineageCacheKFlush policy

--- benchmark/cluster-scripts/run_job.sh
+++ benchmark/cluster-scripts/run_job.sh
@@ -1,10 +1,10 @@
 NUM_RAYLETS=$1
 THROUGHPUT=${2:-100000}
 NUM_REDIS_SHARDS=${3:-1}
-EXPERIMENT_TIME=$3
+LINEAGE_POLICY=${4:-2}
+#EXPERIMENT_TIME=$5
 
-LINEAGE_POLICY=0
-MAX_LINEAGE_SIZE=100
+MAX_LINEAGE_SIZE=1
 GCS_DELAY_MS=-1
 
 HEAD_IP=$(head -n 1 workers.txt)
@@ -16,6 +16,9 @@ elif [ $# -eq 2 ]
 then
 	echo "Running job with $NUM_RAYLETS raylets, lineage policy $LINEAGE_POLICY, GCS delay $GCS_DELAY_MS, throughput $THROUGHPUT, and $NUM_REDIS_SHARDS Redis shards..."
 elif [ $# -eq 3 ]
+then
+	echo "Running job with $NUM_RAYLETS raylets, lineage policy $LINEAGE_POLICY, GCS delay $GCS_DELAY_MS, throughput $THROUGHPUT, and $NUM_REDIS_SHARDS Redis shards..."
+elif [ $# -eq 4 ]
 then
 	echo "Running job with $NUM_RAYLETS raylets, lineage policy $LINEAGE_POLICY, GCS delay $GCS_DELAY_MS, throughput $THROUGHPUT, and $NUM_REDIS_SHARDS Redis shards..."
 else
