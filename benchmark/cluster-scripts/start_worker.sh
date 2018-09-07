469fdfef050e3fc41bc5e30eb45e2326c6b1f43a
Stephanie <swang@cs.berkeley.edu>
2018-09-07 05:17:18 +0000

    Add hugepages to start worker scripts

--- benchmark/cluster-scripts/start_worker.sh
+++ benchmark/cluster-scripts/start_worker.sh
@@ -5,8 +5,13 @@ LINEAGE_POLICY=$4
 MAX_LINEAGE_SIZE=$5
 GCS_DELAY_MS=$6
 
+
 export PATH=/home/ubuntu/anaconda3/bin/:$PATH
 
 ulimit -c unlimited
 ulimit -a
-ray start --num-workers 5 --use-raylet --redis-address=$HEAD_IP:6379 --resources='{"Node'$WORKER_INDEX'": 100}' --gcs-delay-ms $GCS_DELAY_MS --lineage-cache-policy=$LINEAGE_POLICY --max-lineage-size=$MAX_LINEAGE_SIZE
+
+SLEEP_TIME=$(( $RANDOM % 5 ))
+sleep $SLEEP_TIME
+
+ray start --num-workers 5 --use-raylet --redis-address=$HEAD_IP:6379 --resources='{"Node'$WORKER_INDEX'": 100}' --gcs-delay-ms $GCS_DELAY_MS --lineage-cache-policy=$LINEAGE_POLICY --max-lineage-size=$MAX_LINEAGE_SIZE --plasma-directory=/mnt/hugepages --huge-pages
