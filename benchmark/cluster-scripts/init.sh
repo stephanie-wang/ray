f9387bd4defc3b3635b0da5a63d4a9ecb1befec3
Stephanie <swang@cs.berkeley.edu>
2018-09-11 16:07:08 +0000

    Update init.sh to include hugepages and numactl

--- benchmark/cluster-scripts/init.sh
+++ benchmark/cluster-scripts/init.sh
@@ -2,3 +2,6 @@ for host in $(cat workers.txt); do
   ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R $host
   ssh -o "StrictHostKeyChecking no" -i ~/devenv-key.pem $host 'uptime && echo "/home/ubuntu/core.%p" | sudo tee /proc/sys/kernel/core_pattern'
 done
+
+parallel-ssh -h workers.txt -O IdentityFile=~/devenv-key.pem -P -t 0 -I < ~/enable_hugepages.sh
+parallel-ssh -h workers.txt -O IdentityFile=~/devenv-key.pem -P -t 0 "sudo apt-get install numactl"
