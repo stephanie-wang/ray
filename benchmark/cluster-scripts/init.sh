475f9df978eb152bf48229bb1c4a193e05e9dc74
Stephanie <swang@cs.berkeley.edu>
2018-09-19 11:37:48 -0700

    Pin raylet to cores 0,1

--- benchmark/cluster-scripts/init.sh
+++ benchmark/cluster-scripts/init.sh
@@ -2,3 +2,6 @@ for host in $(cat workers.txt); do
   ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R $host
   ssh -o "StrictHostKeyChecking no" -i ~/devenv-key.pem $host uptime
 done
+
+parallel-ssh -h workers.txt -O IdentityFile=~/devenv-key.pem -P -t 0 -I < ~/enable_hugepages.sh
+parallel-ssh -h workers.txt -O IdentityFile=~/devenv-key.pem -P -t 0 "sudo apt-get install numactl"
