527d91bf4fd03e346cece9a5c36396fbd4e8c15a
Stephanie Wang <swang@cs.berkeley.edu>
2020-03-25 20:46:05 -0700

    Mock cluster for video benchmark

--- benchmark/video_benchmark.py
+++ benchmark/video_benchmark.py
@@ -3,8 +3,11 @@ import cv2
 import os.path
 import numpy as np
 import time
+import json
+import threading
 
 import ray
+import ray.cluster_utils
 
 OUTPUT_DIR = "/home/swang/data/images"
 TEST_VIDEO = "/home/swang/data/test.mp4"
@@ -13,11 +16,11 @@ DECODE_CMD = "ffmpeg -i {input} -vf fps={fps} -ss {start} -t {duration} {output_
 
 NUM_FRAMES_PER_CHUNK = 10
 FRAMES_PER_SECOND = 1
-MAX_DURATION = 300.0
+MAX_DURATION = 1200.0
 
 CLEANUP = False
 
-MSE_THRESHOLD = 30
+MSE_THRESHOLD = 100
 
 def get_chunk_dir(chunk_index):
     chunk_dir = os.path.join(OUTPUT_DIR, "image-{}".format(chunk_index))
@@ -28,21 +31,22 @@ def get_chunk_file(chunk_index, frame):
     filename = os.path.join(chunk_dir, "{:03d}.png".format(frame))
     return filename
 
-@ray.remote
+@ray.remote(resources={"query": 1})
 def process_frame(frame):
+    time.sleep(0.1)
     return "a result"
 
-@ray.remote
+@ray.remote(resources={"preprocess": 1})
 def load_frame(filename):
     image = cv2.imread(filename)
     image = cv2.resize(image, (416, 416))
     return image
 
-@ray.remote
+@ray.remote(resources={"preprocess": 1})
 def compute_mse(frame1, frame2):
     return np.square(frame1 - frame2).mean()
 
-@ray.remote
+@ray.remote(resources={"preprocess": 1})
 def process_chunk(chunk_index, num_frames):
     frames = []
     for frame in range(num_frames):
@@ -61,6 +65,8 @@ def process_chunk(chunk_index, num_frames):
         else:
             results.append(process_frame.remote(frame))
             last_frame_index = i
+            if i != 0:
+                print("SWITCHING", i)
     return ray.get(results)
 
 
@@ -113,18 +119,38 @@ def process_video(video_pathname):
 
 
 def main():
-    ray.init()
+    internal_config = json.dumps({
+        "initial_reconstruction_timeout_milliseconds": 200,
+        "num_heartbeats_timeout": 10,
+        "lineage_pinning_enabled": 1,
+    })
+    cluster = ray.cluster_utils.Cluster()
+    cluster.add_node(num_cpus=0, _internal_config=internal_config)
+    preprocess_nodes = [cluster.add_node(num_cpus=2, resources={"preprocess": 100}, _internal_config=internal_config) for _ in range(2)]
+    query_nodes = [cluster.add_node(num_cpus=2, resources={"query": 100}, _internal_config=internal_config) for _ in range(2)]
+    cluster.wait_for_nodes()
+    
 
+    ray.init(address=cluster.address)
     start = time.time()
 
-    cmd = DURATION_CMD.format(TEST_VIDEO).split()
-    duration_s = subprocess.check_output(cmd)
-    duration_s = float(duration_s)
+    #cmd = DURATION_CMD.format(TEST_VIDEO).split()
+    #duration_s = subprocess.check_output(cmd)
+    #duration_s = float(duration_s)
+
+    def kill_node():
+        cluster.remove_node(preprocess_nodes[-1], allow_graceful=False)
+
+    t = threading.Timer(10.0, kill_node)
+    t.start()
+
 
     process_video(TEST_VIDEO)
 
     end = time.time()
-    print(end - start)
+    num_frames = FRAMES_PER_SECOND * MAX_DURATION
+    print("Finished in", end - start)
+    print("Throughput:", num_frames / (end - start))
 
 if __name__ == "__main__":
     main()
