eecdd2f8997dbe2e20c1ab486b79707660c19e9b
Stephanie Wang <swang@cs.berkeley.edu>
2020-03-25 18:02:31 -0700

    First try at video processing

---
+++ benchmark/video_benchmark.py
@@ -0,0 +1,130 @@
+import subprocess
+import cv2
+import os.path
+import numpy as np
+import time
+
+import ray
+
+OUTPUT_DIR = "/home/swang/data/images"
+TEST_VIDEO = "/home/swang/data/test.mp4"
+DURATION_CMD = "ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {}"
+DECODE_CMD = "ffmpeg -i {input} -vf fps={fps} -ss {start} -t {duration} {output_dir}/%03d.png"
+
+NUM_FRAMES_PER_CHUNK = 10
+FRAMES_PER_SECOND = 1
+MAX_DURATION = 300.0
+
+CLEANUP = False
+
+MSE_THRESHOLD = 30
+
+def get_chunk_dir(chunk_index):
+    chunk_dir = os.path.join(OUTPUT_DIR, "image-{}".format(chunk_index))
+    return chunk_dir
+
+def get_chunk_file(chunk_index, frame):
+    chunk_dir = get_chunk_dir(chunk_index)
+    filename = os.path.join(chunk_dir, "{:03d}.png".format(frame))
+    return filename
+
+@ray.remote
+def process_frame(frame):
+    return "a result"
+
+@ray.remote
+def load_frame(filename):
+    image = cv2.imread(filename)
+    image = cv2.resize(image, (416, 416))
+    return image
+
+@ray.remote
+def compute_mse(frame1, frame2):
+    return np.square(frame1 - frame2).mean()
+
+@ray.remote
+def process_chunk(chunk_index, num_frames):
+    frames = []
+    for frame in range(num_frames):
+        frame += 1
+        filename = get_chunk_file(chunk_index, frame)
+        frames.append(load_frame.remote(filename))
+
+    last_frame_index = None
+    results = []
+    for i, frame in enumerate(frames):
+        mse = MSE_THRESHOLD
+        if last_frame_index is not None:
+            mse = ray.get(compute_mse.remote(frame, frames[last_frame_index]))
+        if mse < MSE_THRESHOLD:
+            results.append(results[last_frame_index])
+        else:
+            results.append(process_frame.remote(frame))
+            last_frame_index = i
+    return ray.get(results)
+
+
+def cleanup_chunk(chunk_index, num_frames):
+    chunk_dir = get_chunk_dir(chunk_index)
+    for frame in range(num_frames, 0, -1):
+        filename = get_chunk_file(chunk_index, frame)
+        os.remove(filename)
+    os.rmdir(chunk_dir)
+
+
+def decode_chunk(video_pathname, chunk_index, num_frames, start, duration):
+    chunk_dir = get_chunk_dir(chunk_index)
+    try:
+        os.mkdir(chunk_dir)
+    except FileExistsError:
+        pass
+    last_output = get_chunk_file(chunk_index, num_frames)
+    if not os.path.exists(last_output):
+        cmd = DECODE_CMD.format(
+                input=video_pathname,
+                start=start,
+                fps=FRAMES_PER_SECOND,
+                duration=duration,
+                output_dir=chunk_dir).split()
+        subprocess.check_output(cmd)
+    else:
+        print("Skipping chunk, {} already exists".format(last_output))
+
+def process_video(video_pathname):
+    chunk_duration = float(NUM_FRAMES_PER_CHUNK) / FRAMES_PER_SECOND
+    start = 0
+    chunk_index = 0
+    results = []
+    futures = []
+    while start < MAX_DURATION:
+        decode_chunk(TEST_VIDEO, chunk_index, NUM_FRAMES_PER_CHUNK, start, chunk_duration)
+        if len(futures) >= 6:
+            results += ray.get(futures.pop(0))
+        futures.append(process_chunk.remote(chunk_index, NUM_FRAMES_PER_CHUNK))
+        chunk_index += 1
+        start += chunk_duration
+
+    for f in futures:
+        results += ray.get(f)
+
+    if CLEANUP:
+        for i in range(chunk_index):
+            cleanup_chunk(i, NUM_FRAMES_PER_CHUNK)
+
+
+def main():
+    ray.init()
+
+    start = time.time()
+
+    cmd = DURATION_CMD.format(TEST_VIDEO).split()
+    duration_s = subprocess.check_output(cmd)
+    duration_s = float(duration_s)
+
+    process_video(TEST_VIDEO)
+
+    end = time.time()
+    print(end - start)
+
+if __name__ == "__main__":
+    main()
