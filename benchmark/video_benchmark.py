c8503f509264e233d7bd9db927b83a1be89280a2
Stephanie Wang <swang@cs.berkeley.edu>
2020-04-22 17:58:05 -0700

    Tweaks for cluster version of video benchmark

--- benchmark/video_benchmark.py
+++ benchmark/video_benchmark.py
@@ -1,18 +1,14 @@
-import subprocess
 import cv2
 import os.path
 import numpy as np
 import time
 import json
 import threading
-import tempfile
-import sys
 from ray import profiling
 
 import ray
 import ray.cluster_utils
 
-OUTPUT_DIR = "/home/swang/data/images"
 TEST_VIDEO = "/home/swang/data/test.mp4"
 YOLO_PATH = "/home/swang/darknet"
 
@@ -24,12 +20,6 @@ CLEANUP = False
 MSE_THRESHOLD = 70
 
 
-def get_chunk_file(chunk_index, frame):
-    frame = chunk_index * NUM_FRAMES_PER_CHUNK + frame
-    filename = os.path.join(OUTPUT_DIR, "image-{:06d}.png".format(frame))
-    return filename
-
-
 def load_model():
     global net, ln
     weightsPath = os.path.join(YOLO_PATH, "yolov3-tiny.weights")
@@ -39,7 +29,7 @@ def load_model():
     ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 
-@ray.remote(resources={"query": 1})
+@ray.remote(num_gpus=1)
 def process_frame(frame):
     global net, ln
     if "net" not in globals():
@@ -62,6 +52,7 @@ def process_frame(frame):
 
 @ray.remote(resources={"preprocess": 1})
 def load_frames(filename, start_frame, num_frames):
+    filename = filename.decode("ascii")
     v = cv2.VideoCapture(filename)
     v.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
     frames = []
@@ -91,7 +82,11 @@ def process_chunk_single_thread(filename, start_frame, num_frames):
             assert grabbed
             # Use uint8_t to reduce image size.
             frame = cv2.dnn.blobFromImage(
-                frame, 1, (416, 416), swapRB=True, crop=False, ddepth=cv2.CV_8U)
+                frame,
+                1, (416, 416),
+                swapRB=True,
+                crop=False,
+                ddepth=cv2.CV_8U)
             frames.append(frame)
 
     last_frame_index = None
@@ -132,12 +127,6 @@ def process_chunk(filename, start_frame, num_frames):
     return ray.get(results)
 
 
-def cleanup_chunk(chunk_index, num_frames):
-    for frame in range(num_frames, 0, -1):
-        filename = get_chunk_file(chunk_index, frame)
-        os.remove(filename)
-
-
 def process_video(video_pathname, num_total_frames):
     results = []
     futures = []
@@ -154,60 +143,67 @@ def process_video(video_pathname, num_total_frames):
     for f in futures:
         results += ray.get(f)
 
-    if CLEANUP:
-        for i in range(chunk_index):
-            cleanup_chunk(i, NUM_FRAMES_PER_CHUNK)
 
-
-def main(test_failure, timeline):
-    internal_config = json.dumps({
-        "initial_reconstruction_timeout_milliseconds": 100000,
-        "num_heartbeats_timeout": 10,
-        "lineage_pinning_enabled": 1,
-        "free_objects_period_milliseconds": -1,
-        "object_manager_repeated_push_delay_ms": 1000,
-        "task_retry_delay_ms": 100,
-    })
-    cluster = ray.cluster_utils.Cluster()
-    cluster.add_node(
-        num_cpus=0, _internal_config=internal_config, include_webui=False)
-    preprocess_nodes = [
+def main(video_path, local, test_failure, timeline):
+    if local:
+        internal_config = json.dumps({
+            "initial_reconstruction_timeout_milliseconds": 100000,
+            "num_heartbeats_timeout": 10,
+            "lineage_pinning_enabled": 1,
+            "free_objects_period_milliseconds": -1,
+            "object_manager_repeated_push_delay_ms": 1000,
+            "task_retry_delay_ms": 100,
+        })
+        cluster = ray.cluster_utils.Cluster()
         cluster.add_node(
-            object_store_memory=10**9,
-            num_cpus=2,
-            resources={"preprocess": 100},
-            _internal_config=internal_config) for _ in range(2)
-    ]
-    query_nodes = [
-        cluster.add_node(
-            object_store_memory=10**9,
-            num_cpus=2,
-            resources={"query": 100},
-            _internal_config=internal_config) for _ in range(1)
-    ]
-    cluster.wait_for_nodes()
+            num_cpus=0, _internal_config=internal_config, include_webui=False)
+        for _ in range(2):
+            cluster.add_node(
+                object_store_memory=10**9,
+                num_cpus=2,
+                _internal_config=internal_config)
+        for _ in range(1):
+            cluster.add_node(
+                object_store_memory=10**9,
+                num_gpus=2,
+                _internal_config=internal_config)
+        cluster.wait_for_nodes()
+        address = cluster.address
+    else:
+        address = "auto"
 
-    ray.init(address=cluster.address)
-    start = time.time()
+    ray.init(address=address, _internal_config=internal_config)
 
-    v = cv2.VideoCapture(TEST_VIDEO)
+    nodes = ray.nodes()
+    for node in nodes:
+        if "GPU" not in node["Resources"] and "CPU" in node["Resources"]:
+            ray.experimental.set_resource("preprocess", 100, node["NodeID"])
+
+    v = cv2.VideoCapture(video_path)
     num_total_frames = min(v.get(cv2.CAP_PROP_FRAME_COUNT), MAX_FRAMES)
     print("FRAMES", num_total_frames)
-    t = threading.Thread(
-        target=process_video, args=(TEST_VIDEO, num_total_frames))
-    t.start()
 
-    if test_failure:
-        time.sleep(3)
-        cluster.remove_node(preprocess_nodes[-1], allow_graceful=False)
-        time.sleep(1)
-        cluster.add_node(
-            object_store_memory=10**9,
-            num_cpus=2,
-            resources={"preprocess": 100},
-            _internal_config=internal_config)
+    start = time.time()
 
-    t.join()
+    if local and test_failure:
+        t = threading.Thread(
+            target=process_video, args=(video_path, num_total_frames))
+        t.start()
+
+        if test_failure:
+            time.sleep(3)
+            cluster.remove_node(preprocess_nodes[-1], allow_graceful=False)
+            time.sleep(1)
+            cluster.add_node(
+                object_store_memory=10**9,
+                num_cpus=2,
+                resources={"preprocess": 100},
+                _internal_config=internal_config)
+
+        t.join()
+    else:
+        video_path = video_path.encode("ascii")
+        process_video(video_path, num_total_frames)
 
     end = time.time()
     print("Finished in", end - start)
@@ -219,9 +215,11 @@ def main(test_failure, timeline):
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description='Run the video benchmark.')
+    parser = argparse.ArgumentParser(description="Run the video benchmark.")
 
+    parser.add_argument("--video-path", required=True, type=str)
+    parser.add_argument("--local", action="store_true")
     parser.add_argument("--failure", action="store_true")
     parser.add_argument("--timeline", default=None, type=str)
     args = parser.parse_args()
-    main(args.failure, args.timeline)
+    main(args.video_path, args.local, args.failure, args.timeline)
