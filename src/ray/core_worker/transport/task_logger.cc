13b5903b644d88b7ef23f0f4a9d66e0e78f5f0f2
Audrey Cheng <accheng@c02d56vvmd6r.hsd1.ca.comcast.net>
2020-11-20 15:04:43 -0800

    fixed logging

--- src/ray/core_worker/transport/task_logger.cc
+++ src/ray/core_worker/transport/task_logger.cc
@@ -9,8 +9,11 @@ namespace ray {
 void TaskLogger::LogRequest(const rpc::PushTaskRequest &request, const WorkerID worker_id) {
   RAY_LOG(DEBUG) << "Logging request";
   std::ofstream persistent;
-  persistent.open("~/Documents/ray-src/worker_log_" + worker_id.Binary() + ".txt");
+  persistent.open("/Users/accheng/Documents/ray_source/worker_log_" + worker_id.Hex() + ".txt",
+  	std::ofstream::out | std::ofstream::app | std::ios::binary);
+  RAY_CHECK(persistent.good());
   request.SerializeToOstream(&persistent);
+  RAY_CHECK(persistent.good());
   persistent.close();
   log_.push_back(request);
 }
