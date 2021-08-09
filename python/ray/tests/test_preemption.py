5bde102a083be49efd61a00d50d10b771a9819d2
Stephanie Wang <swang@cs.berkeley.edu>
2021-08-09 11:44:39 -0700

    simple test for preemption

--- python/ray/tests/test_preemption.py
+++ python/ray/tests/test_preemption.py
@@ -1,6 +1,7 @@
 import os
 import signal
 import sys
+import time
 
 import numpy as np
 import pytest
@@ -11,23 +12,40 @@ from ray.test_utils import (
     wait_for_pid_to_exit,
 )
 
-def test_depth(ray_start_cluster):
+SIGKILL = signal.SIGKILL if sys.platform != "win32" else signal.SIGTERM
+
+
+def test_preemption_simple(ray_start_cluster):
+    config = {
+        "lineage_pinning_enabled": True,
+        "max_direct_call_object_size": 10,
+    }
     cluster = ray_start_cluster
     # Head node with no resources.
-    cluster.add_node(num_cpus=1)
-    ray.init(address=cluster.address)
+    cluster.add_node(num_cpus=1, _system_config=config)
+
+    ray.init(cluster.address)
 
     @ray.remote
-    def f(x):
-        return x
+    def f():
+        time.sleep(1)
 
+    xs = [f.remote() for _ in range(3)]
+    ray.get(xs)
+    start = time.time()
+    ray.get(xs, timeout=0)
+    end = time.time()
+    print("first get done", end - start)
 
-    x = ray.put(1)
-    for i in range(10):
-        print(i, x)
-        x = f.remote(x)
-    ray.get(x)
+    worker = ray.worker.global_worker
+    worker.core_worker.preempt(xs[0])
+    start = time.time()
+    print(ray.get(xs))
+    end = time.time()
+    print("DONE", end - start)
 
+    # Make sure the task actually reran.
+    assert (end - start) >= 1
 
 if __name__ == "__main__":
     import pytest
